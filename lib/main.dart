aef2f1a6ca869f1ef837202acd0347eafbe97fc5
Ahmed-Dlshad1212 <149675381+Ahmed-Dlshad1212@users.noreply.github.com>
2024-01-17 23:52:01 +0300

    app code

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_application_55/firebase_options.dart';
+import 'package:flutter_application_55/views/onboarding_screen.dart';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context)
+              .textTheme, // If this is not set, then ThemeData.light().textTheme is used.
+        ),
+      ),
+      title: 'Flutter app',
+      home: const OnBoardingScreen(),
+    );
+  }
+}
